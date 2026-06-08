694a66f9fcc7716f5137f1c31eae15c92f5895da
KANISHK RAJ <kanishkraj601@gmail.com>
2026-06-08 15:20:35 +0530

    Update Day3 operators and Day4 Q1, Q2 solutions

--- Day4/Q2.py
+++ Day4/Q2.py
@@ -1,9 +1,9 @@
 product = input("Enter a product name: ")
 quantity = int(input("Enter a quantity: "))
-price = int(input("Enter a price: "))
-total_cost = quantity *price
-gst =total_cost * 0.18
-final_bill = total_cost+gst
+price= int(input("Enter a price: "))
+total_cost =quantity *price
+gst =total_cost*0.18
+final_bill =total_cost+gst
 
 print("Total Cost",total_cost)
 print("GST",gst)
